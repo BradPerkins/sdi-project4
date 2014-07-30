8eb0045d5d9669bc8f12a67e274ff16770637fc0
Brad Perkins <bperkins99@fullsail.edu>
2014-07-30 12:11:25 -0400

    String follow a 123-456-7890 pattern
    Does String follow a 123-456-7890 pattern like a phone number? finished.

--- sdi-project4.js
+++ sdi-project4.js
@@ -3,3 +3,27 @@
 // SDI 1407
 // 07-29-2014
 
+
+// Does String follow a 123-456-7890 pattern like a phone number?
+
+var phoneNumber = function(number) {
+	var newNum = number;
+	var originalNumber = "123-456-7890";
+	if (number.charAt(3) === "-" && number.charAt(7) === "-" && number.length === originalNumber.length) {
+		return (newNum + " is a real phone number");
+	}
+	else {
+		return ( newNum + " is not a real phone number");
+	}
+};
+
+
+console.log(phoneNumber("222-222-222"));
+
+
+// Does a string follow an aaa@bbb.ccc pattern like an email address?
+
+var emailPattern = function(email) {
+	var newEmail = email;
+	var originalEmail
+};
