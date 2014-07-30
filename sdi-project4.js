0ea2da814bc4a553f6f7e579dc33bc1ab2e61e73
Brad Perkins <bperkins99@fullsail.edu>
2014-07-30 13:13:33 -0400

    finished email string
    Does a string follow an aaa@bbb.ccc pattern like an email address?

--- sdi-project4.js
+++ sdi-project4.js
@@ -3,7 +3,7 @@
 // SDI 1407
 // 07-29-2014
 
-
+/*
 // Does String follow a 123-456-7890 pattern like a phone number?
 
 var phoneNumber = function(number) {
@@ -20,10 +20,51 @@ var phoneNumber = function(number) {
 
 console.log(phoneNumber("222-222-222"));
 
+*/
+
 
 // Does a string follow an aaa@bbb.ccc pattern like an email address?
 
+// index of @ has to be before . 
+
+
 var emailPattern = function(email) {
 	var newEmail = email;
-	var originalEmail
+	if (email.indexOf("@") > 0 && email.indexOf("@") < email.indexOf(".") && email.indexOf(".") > email.indexOf("@")){
+		return (newEmail + " is in the right email format");
+	}
+	else {
+		return ( newEmail + " is not in the right email format");
+	}
 };
+
+console.log(emailPattern("bperkins99@fullsail.edu"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
